bd7e6dee9819731ed495fc4cc72abaa8500ba769
root <root@lamp.us-central1-a.c.games-crytera.internal>
2021-05-13 01:40:21 +0000

    Fix routings

--- index.php
+++ index.php
@@ -5,8 +5,9 @@ require_once 'libs/Controller.php';
 require_once 'libs/Model.php';
 require_once 'libs/View.php';
 require_once 'libs/App.php';
-require_once 'config/config.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+#requires env variable
+require_once 'config/config.php';
 $app = new App();
